e5e994bbc847b9ef4834573182cae65ef7935341
peixoart <arthur.peixoto18@outlook.fr>
2025-11-05 05:29:19 -0500

    json

--- get-artifacts1.sh
+++ get-artifacts1.sh
@@ -2,4 +2,12 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: $(cat ../token.gh)" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/peixoart/graphes/actions/artifacts
+  https://api.github.com/repos/peixoart/graphes/actions/artifacts > gh-artifacts.json
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: $(cat ../token.gh)" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/peixoart/graphes/actions/artifacts/4472412512/zip > gh-artifacts.json
+  --output artéfact.zip \
+  unzip artéfact.zip \
